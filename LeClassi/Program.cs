e5f446de115d7568efc9d902fd41e41e655d6f5b
mario <mario@WIMDOWS_MARIO>
2023-03-09 15:14:40 +0100

    Aggiungere i file di progetto.

---
+++ LeClassi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeClassi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
